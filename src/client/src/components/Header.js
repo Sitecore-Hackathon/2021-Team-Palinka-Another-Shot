11d67c8541e63c100443860d619a54fc872f8570
Peter Schmiz <peter.schmiz@possible.com>
2021-03-06 13:57:40 +0100

    [master] feat(build): add mocked data, add fetch for workflow and filters

--- src/client/src/components/Header.js
+++ src/client/src/components/Header.js
@@ -1,14 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchWorkflows, workflowSelector } from "../redux/slices/workflows";
+import { fetchWorkItems, workItemsSelector } from "../redux/slices/workitems";
 import Select from "react-select";
 import Logo from "../assets/logo.png";
 
-const options = [
-  { value: "workflow1", label: "Standard" },
-  { value: "workflow2", label: "Editorial" },
-  { value: "workflow3", label: "External" }
-];
-
 const Header = () => {
+  const dispatch = useDispatch();
+  const { workflowItems, loading } = useSelector(workflowSelector);
+  const { workItems } = useSelector(workItemsSelector);
+  const [selectedOptionId, setSelectedOptionId] = useState(0);
+
+  useEffect(() => {
+    dispatch(fetchWorkflows());
+  }, [dispatch]);
+
+  const onChange = (option, { action }) => {
+    if (action === "select-option") {
+      dispatch(fetchWorkItems(option.Id));
+    }
+  };
+
+  const renderQuickFilters = () => {
+    if (selectedOptionId === 0 && workflowItems.length > 0) {
+      const selectedOption = workflowItems.filter(option => option.IsSelected === true);
+      setSelectedOptionId(selectedOption[0].Id);
+      dispatch(fetchWorkItems(selectedOptionId));
+    }
+
+    if (!workItems.QuickFilters) {
+      return (
+        <div className="col col-no-gutter">&nbsp;</div>
+      );
+    }
+
+    return (
+      <>
+        <h6>Quick filters:</h6>
+        <div className="col col-no-gutter">
+          <ul className="quick-filters">
+            {
+              Object.keys(workItems.QuickFilters || {}).map(key => {
+                return (
+                  <li key={key}>
+                    <button>{key}</button>
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </div>
+      </>
+    );
+  };
+
   return (
     <header className="header">
       <div className="header__top row middle-xs between-xs">
@@ -16,26 +61,23 @@ const Header = () => {
           <img src={Logo} alt="Advanced Workbox Logo"/>
           <h1>Advanced Workbox</h1>
         </div>
+      </div>
+      <div className="header__bottom row middle-xs start-xs">
+        {renderQuickFilters()}
+        {workflowItems.length > 0 &&
         <Select
           className="workflow-selector"
           placeholder="Select workflow"
+          isLoading={loading}
           isClearable={false}
           isSearchable={false}
-          options={options}
+          onChange={onChange}
+          options={workflowItems}
+          defaultValue={workflowItems.filter(option => option.IsSelected === true)}
+          getOptionLabel={opt => opt.Name}
+          getOptionValue={opt => opt.Id}
         />
-      </div>
-      <div className="header__bottom row middle-xs start-xs">
-        <h6>Quick filters:</h6>
-        <div className="col col-no-gutter">
-          <ul className="quick-filters">
-            <li>
-              <button>Recently updated</button>
-            </li>
-            <li>
-              <button>Another filter</button>
-            </li>
-          </ul>
-        </div>
+        }
       </div>
     </header>
   );
--- src/client/src/redux/slices/index.js
+++ src/client/src/redux/slices/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import workItemsReducer from "./workitems";
+import workflowsReducer from "./workflows";
 
 const rootReducer = combineReducers({
   workItems: workItemsReducer,
+  workflowItems: workflowsReducer
 });
 
 export default rootReducer;
