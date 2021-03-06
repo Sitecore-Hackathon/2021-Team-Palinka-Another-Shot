86d561206d7ea551afc43e288a5f68d302c537e9
Peter Schmiz <peter.schmiz@possible.com>
2021-03-06 16:55:05 +0100

    [master] feat(build): add next state check and visual highlight

--- src/client/src/components/Column.js
+++ src/client/src/components/Column.js
@@ -4,9 +4,16 @@ import WorkItem from "./WorkItem";
 
 const Column = (props) => {
   return (
-    <Droppable droppableId={props.column.Id}>
+    <Droppable
+      droppableId={props.column.Id}
+      isDropDisabled={props.isDropDisabled}
+    >
       {(provided, snapshot) => (
-        <div className={`col-md col-xs-12 board-column ${snapshot.isDraggingOver ? "board-column--is-dragging-over" : ""}`}>
+        <div className={
+          `col-md col-xs-12 board-column
+          ${snapshot.isDraggingOver && !props.noHighlight ? "board-column--is-dragging-over" : ""}
+          ${!props.isDropDisabled && !props.noHighlight? "board-column--dropping-enabled" : ""}
+          `}>
           <h3>{props.column.Name}</h3>
           <div ref={provided.innerRef} {...provided.droppableProps}>
             {props.items.map((item, index) => (
